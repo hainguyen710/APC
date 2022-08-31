43e012b70d269b5c9479459e28a88ee5902f84c0
hainguyen710 <HaiNHTH2110016@fpt.edu.vn>
2022-08-31 10:36:34 +0700

    1

---
+++ Cylinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeomytryExample
+{
+    internal class Cylinder
+    {
+        double height;
+        double radius;
+
+
+        public class Cylinder(double height, double radius){
+            this
+            }
+
+
+
+
+        public BaseArea()
+        {
+            
+        }
+    }
+}
